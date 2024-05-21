9e6a9557f63fbb4c9b40fac4e9c8569c39d95530
zvdv <156561301+zvdv@users.noreply.github.com>
2024-05-20 22:00:25 -0700

    Start simple bidding architecture

--- bid.sol
+++ bid.sol
@@ -1,1 +1,32 @@
+// SPDX-License-Identifier: GPL-3.0
+
 pragma solidity >=0.8.2 <0.9.0;
+import "hardhat/console.sol";
+
+contract auctionBids {
+    struct Bid {
+        address bidder;
+        uint bid;
+    }
+    Bid[] bids;
+    uint i;
+
+    constructor() {
+        i = 0;
+    }
+
+    function makeBid(address addr, uint value) public {
+        bids[i] = Bid(addr, value);
+        i++;
+    }
+
+    function printBids() public pure {
+        // address bidder0 = bids[0].bidder;
+        // uint bid0 = bids[0].bid;
+
+        console.log(
+            "Hello"
+        );
+    }
+
+}
