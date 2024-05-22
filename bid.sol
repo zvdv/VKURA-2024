8aed3785a4529ecd1de11a2451b083d367078fde
zvdv <156561301+zvdv@users.noreply.github.com>
2024-05-22 12:14:31 -0700

    Add closeAuction and newAuction functions

--- bid.sol
+++ bid.sol
@@ -4,29 +4,61 @@ pragma solidity >=0.8.2 <0.9.0;
 import "hardhat/console.sol";
 
 contract auctionBids {
-    struct Bid {
-        address bidder;
-        uint bid;
-    }
-    Bid[] bids;
+    address payable admin;
+    address payable[] bidders;
+    uint[] bids;
     uint i;
 
     constructor() {
         i = 0;
+        admin = payable(msg.sender);
     }
 
-    function makeBid(address addr, uint value) public {
-        bids[i] = Bid(addr, value);
+    modifier onlyAdmin() {
+        require(msg.sender == admin);
+        _;
+    }
+
+    function makeBid(address payable addr) public payable {
+        bidders[i] = addr;
+        bids[i] = msg.value;
         i++;
     }
 
-    function printBids() public pure {
-        // address bidder0 = bids[0].bidder;
-        // uint bid0 = bids[0].bid;
+    function closeAuction() public onlyAdmin {
+        uint maxBid = 0;
+        uint maxIndex = 0;
 
-        console.log(
-            "Hello"
-        );
+        // Find highest bid
+        for (uint j = 0; j < bids.length; j++){
+            if (bids[j] > maxBid){
+                maxBid = bids[j];
+                maxIndex = j;
+            }
+        }
+
+        // Return bids that aren't the highest
+        for (uint k = 0; k < bids.length; k++){
+            if (k != maxIndex){
+                bidders[k].transfer(bids[k]);
+            }
+        }
+
+        // Transfer highest bid to Admin (who would transfer whatever's being auctioned to the winner)
+        admin.transfer(bids[maxIndex]);
+    }    
+
+    function newAuction() public onlyAdmin {
+        // Clear bidder and bid arrays
+        delete bidders;
+        delete bids;
+        i = 0;
     }
 
+    function printBids() public view {
+        // Print first couple bids
+        console.log("First Bid:", bidders[0], " ", bids[0]);
+        console.log("Second Bid:", bidders[1], " ", bids[1]);
+        console.log("Third Bid:", bidders[2], " ", bids[2]);
+    }
 }
