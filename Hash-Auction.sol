48f89ef0ebe826ca7963e9fa258e63d7f84d5a08
zvdv <156561301+zvdv@users.noreply.github.com>
2024-07-12 12:45:51 -0700

    Auction end logic

--- Hash-Auction.sol
+++ Hash-Auction.sol
@@ -3,6 +3,8 @@
 pragma solidity ^0.8.26;
 
 contract Auction {
+    bool public open;
+
     struct Bidder {
         //bool exists;
         bool paidBack;
@@ -21,15 +23,16 @@ contract Auction {
     //string public auctioneerPublicKey;
     uint public fairFee; // Minimum deposit for auctioneer and bidders to ensure fairness
     uint public bidPeriodEnd; // Number of blocks
-    uint public revealPeriodEnd; // Number of blocks
-    uint public claimWinnerEnd; // Number of blocks
+    uint public revealPeriodEnd;
+    uint public claimWinnerEnd;
+    uint public withdrawEnd;
     //uint public maxBidders;
     //uint public maxBid;
 
     address public winner;
     uint public winningBid;
 
-    constructor(uint _fairFee, uint bidPeriod, uint revealPeriod, uint claimWinnerPeriod) payable {
+    constructor(uint _fairFee, uint bidPeriod, uint revealPeriod, uint claimWinnerPeriod, uint withdrawPeriod) payable {
         require(msg.value >= _fairFee, "Insufficient deposit.");
         auctioneerPaid = msg.value;
         auctioneerAddress = msg.sender;
@@ -37,6 +40,7 @@ contract Auction {
         bidPeriodEnd = block.number + bidPeriod;
         revealPeriodEnd = bidPeriodEnd + revealPeriod;
         claimWinnerEnd = revealPeriodEnd + claimWinnerPeriod;
+        withdrawEnd = claimWinnerEnd + withdrawPeriod;
         //maxBidders = _maxBidders;
     }
 
@@ -75,11 +79,24 @@ contract Auction {
     }
 
     function withdraw() public {
-        require(block.number > claimWinnerEnd, "Too early to withdraw.");
+        require(block.number > claimWinnerEnd && block.number < withdrawEnd, "Outside withdrawal period.");
         uint amount = bids[msg.sender].paid;
         bids[msg.sender].paid = 0;
         payable(msg.sender).transfer(amount);
     }
+
+    function endAuction() public OnlyAuctioneer {
+        require(block.number > withdrawEnd, "Too early to close auction.");
+        uint amount = auctioneerPaid;
+        auctioneerPaid = 0;
+        payable(auctioneerAddress).transfer(amount);
+        open = true;
+        // Clear all variables
+    }
+
+    // Make function mirroring constructor to start a new auction
+    // Require open == true
+    // Set 'open' bool back to false
 
     modifier OnlyAuctioneer(){
         require(msg.sender == auctioneerAddress, "Only auctioneer can call.");
