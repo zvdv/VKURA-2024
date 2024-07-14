460a24eda52986b666e51a8583ab954863dcd3c1
zvdv <156561301+zvdv@users.noreply.github.com>
2024-07-13 20:43:09 -0700

    Began contract access from page.js

--- encode.mjs
+++ encode.mjs
@@ -2,7 +2,7 @@ import Web3 from 'web3'
 
 const web3 = new Web3(Web3.givenProvider || new Web3.providers.HttpProvider('https://rpc.sepolia.org'));
 
-let encoded = web3.eth.abi.encodeParameters(['uint', 'uint'], ['100','97']);
+let encoded = web3.eth.abi.encodeParameters(['uint', 'uint'], ['240','323']);
 console.log(encoded);
 
 let hashed = web3.utils.soliditySha3(encoded);
