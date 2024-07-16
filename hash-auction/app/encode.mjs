8a180dbf6434ca77daac1ab516797375b04e8531
zvdv <156561301+zvdv@users.noreply.github.com>
2024-07-15 21:17:06 -0700

    Hasher component

---
+++ hash-auction/app/encode.mjs
@@ -0,0 +1,33 @@
+import React from 'react'
+import Web3 from 'web3'
+
+const web3 = new Web3(Web3.givenProvider || new Web3.providers.HttpProvider('https://rpc.sepolia.org'));
+
+// let encoded = web3.eth.abi.encodeParameters(['uint', 'uint'], ['240','323']);
+// console.log(encoded);
+
+// let hashed = web3.utils.soliditySha3(encoded);
+// console.log(hashed);
+
+function getRndInteger(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) ) + min;
+}
+
+function hash(bid){
+    let nonce = getRndInteger(1000,1000000000);
+    let encoded = web3.eth.abi.encodeParameters(['uint', 'uint'], [bid,nonce]);
+    let hashed = web3.utils.soliditySha3(encoded);
+    return hashed;
+}
+
+export default function Hasher() {
+    let result;
+    return (
+        <div>
+            <label for="bid">Enter bid:</label>
+            <input type="number" id="bid" name="bid"></input>
+            <button onClick={result = hash(document.getElementById("bid").value)}>Hash bid with nonce</button>
+            <p>Hashed value: {result}</p>
+        </div>
+    )
+  }
