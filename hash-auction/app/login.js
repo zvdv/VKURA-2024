bcb9d20eb1192b64bfadbddefd68ad4e3be6ba35
zvdv <156561301+zvdv@users.noreply.github.com>
2024-07-16 20:08:55 -0700

    Login page

---
+++ hash-auction/app/login.js
@@ -0,0 +1,27 @@
+'use client'
+import { useRouter } from 'next/navigation';
+import React from 'react';
+import { isAddress } from 'web3-validator';
+
+export default function Login() {
+    const router = useRouter();
+
+    function validateAddress(){
+        if (isAddress(document.getElementById('address').value)){
+            router.push('/dashboard')
+        } else {
+            alert("Please enter a valid Ethereum address.")
+        }
+    }
+
+    return (
+      <div class='centerdiv'>
+        <form action={validateAddress}>
+            <label for='address'>Ethereum address: </label>
+            <input type='text' id='address' name='address' />
+            <br />
+            <input type='submit' value='Login'/>
+        </form>
+      </div>
+    );
+  }
--- hash-auction/app/page.js
+++ hash-auction/app/page.js
@@ -1,27 +1,8 @@
-'use client'
-import { useRouter } from 'next/navigation';
 import React from 'react';
-import { isAddress } from 'web3-validator';
+import Login from './login';
 
-export default function Login() {
-    const router = useRouter();
-
-    function validateAddress(){
-        if (isAddress(document.getElementById('address').value)){
-            router.push('/dashboard')
-        } else {
-            alert("Please enter a valid Ethereum address.")
-        }
-    }
-
-    return (
-      <div class='centerdiv'>
-        <form action={validateAddress}>
-            <label for='address'>Ethereum address: </label>
-            <input type='text' id='address' name='address' />
-            <br />
-            <input type='submit' value='Login' className='cursor-pointer'/>
-        </form>
-      </div>
-    );
-  }
+export default function page(){
+    return(
+        <Login />
+    )
+}
