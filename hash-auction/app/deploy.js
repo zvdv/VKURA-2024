5f4df31e219885f6a3efe543507546ea8236cc76
zvdv <156561301+zvdv@users.noreply.github.com>
2024-08-11 19:06:07 -0700

    Setting up values for eth_sendTransaction

--- hash-auction/app/deploy.js
+++ hash-auction/app/deploy.js
@@ -5,8 +5,6 @@ import web3 from './setweb3';
 import { useSessionStorage } from 'usehooks-ts';
 import { contractToDeploy } from './setweb3';
 
-const method = "eth_signTypedData_v4"
-
 async function deployContract(auctioneerAddress){
     let fairFee = document.getElementById('fairFee').value;
     let bidPeriod = document.getElementById('bidPeriod').value;
@@ -20,7 +18,7 @@ async function deployContract(auctioneerAddress){
     } else {
         testing = radio[1].value;
     }
-    let msgvalue = document.getElementById('value').value;
+    let msgvalue = document.getElementById('weivalue').value;
 
     console.log("Successfully declared variables!");
 
@@ -31,29 +29,62 @@ async function deployContract(auctioneerAddress){
     // const gas = await deployer.estimateGas(function(err, gas){
     //     console.log(gas);
     // });
-    const gas = 2000000;
-    //console.log("Successfully estimated gas!");
+    // // // // 1449000 estimate from Remix
+    const gas = 2000000; // Could make this user-settable
+    const gasPrice = 2000000000;
 
     console.log(auctioneerAddress);
     let tx;
 
     // Sign transaction!
-    console.log(method);
+    const data = deployer.encodeABI();
 
     try {
-        console.log("In try")
-        tx = await deployer.send({
-            from: auctioneerAddress,
-            gas: gas,
-            gasPrice: 10000000000,
-            value: msgvalue,
-        });
-        console.log("Sent deploy!")
-        document.getElementById('reply').innerHTML = "Deployed at address: " + tx.options.address;
-    } catch (error) {
-        console.log(error);
-        document.getElementById('error').innerHTML = "Log: " + error;
+        console.log("In try");
+        console.log("Testing: " + testing + " type " + typeof testing);
+        console.log("Fair fee: " + fairFee + " type " + typeof fairFee);
+        console.log("Bid period: " + bidPeriod + " type " + typeof bidPeriod);
+        console.log("Reveal period: " + revealPeriod + " type " + typeof revealPeriod);
+        console.log("Withdraw period: " + withdrawPeriod + " type " + typeof withdrawPeriod);
+        console.log("Gas: " + gas + " type " + typeof gas);
+        console.log("from: " + auctioneerAddress);
+        console.log("gas hex: " + gas.toString(16));
+        console.log("Value: " + msgvalue + " type " + typeof msgvalue);
+        console.log("value hex: " + msgvalue.toString(16));
+        //console.log("data: " + data);
+        console.log("gasPrice hex:" + gasPrice.toString(16));
+        
+        // tx = await window.ethereum.request({
+        //     "method": "eth_sendTransaction",
+        //     "params": [
+        //       {
+        //         // No "to" because contract creation
+        //         "from": auctioneerAddress,
+        //         "gas": gas.toString(16),
+        //         "value": msgvalue.toString(16),
+        //         "data": data,
+        //         "gasPrice": gasPrice.toString(16)
+        //       }
+        //     ]
+        //   });
+    } catch(error) {
+        console.error("Error deploying: " + error);
     }
+
+    // try {
+    //     console.log("In try");
+    //     tx = await deployer.send({
+    //         from: auctioneerAddress,
+    //         gas: gas,
+    //         gasPrice: 10000000000,
+    //         value: msgvalue,
+    //     });
+    //     console.log("Sent deploy!")
+    //     document.getElementById('reply').innerHTML = "Deployed at address: " + tx.options.address;
+    // } catch (error) {
+    //     console.log(error);
+    //     document.getElementById('error').innerHTML = "Log: " + error;
+    // }
     
     //return tx.options.contractAddress;
 }
@@ -85,8 +116,8 @@ export default function Deployer({address}){
             <input type='radio' id='false' name='testing' value={false} required />
             <label htmlFor='false'>No</label>
             <br />
-            <label htmlFor='value'>Value (wei): </label>
-            <input type='number' id='value' name='value' required />
+            <label htmlFor='weivalue'>Value (wei): </label>
+            <input type='number' id='weivalue' name='weivalue' required />
             <br />
             <input type='submit' />
         </form>
